d872b670e0f71692d3b953c1c15927b413b82789
Anshu Ahirwar <Ahirwar.Anshu@con-way.com>
2023-02-22 14:31:28 +0530

    sidebar complete

--- headerSideNav/src/app/sidebar/sidebar.component.ts
+++ headerSideNav/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -51,6 +51,8 @@ export class SidebarComponent implements OnInit {
       link: 'home'
     },
   ];
+
+  @Input() sideNavStatus: boolean = true;
 
   constructor() {}
 
